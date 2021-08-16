2e48429d7c249d1fd41ab1733ff014ce4653a1bd
cohenchris <chris@chriscohen.dev>
2021-08-16 16:20:25 -0700

    fixed huge font lol

--- config.def.h
+++ config.def.h
@@ -5,8 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-//static char *font = "Liberation Mono:pixelsize=30:antialias=true:autohint=true";
-static char *font = "Terminus (TTF):pixelsize=30:antialias=true:autohint=true";
+static char *font = "Terminus (TTF):pixelsize=17:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
