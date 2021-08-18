620119f8bce169583a9786fa244a2a7f31cd7625
cohenchris <chris@chriscohen.dev>
2021-08-17 22:26:35 -0700

    adjusted font size

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Terminus (TTF):pixelsize=17:antialias=true:autohint=true";
+static char *font = "Terminus (TTF):pixelsize=15:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
